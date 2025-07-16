950feefd51437ab7b91bcbc302b18eca03c3c579
Olivier GOY <olivier.goy.37@gmail.com>
2025-07-16 18:21:44 +0200

    commit for push work of end days

--- Pokenative/app/index.tsx
+++ Pokenative/app/index.tsx
@@ -1,15 +1,30 @@
+import { useThemeColors } from '@/app/pokemon/hooks/useThemeColors';
 import { Card } from '@/components/Card';
 import { ThemedText } from '@/components/ThemedText';
-import { StyleSheet } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useThemeColors } from './pokemon/hooks/useThemeColors';
 
 export default function Index() {
   const colors = useThemeColors();
+  const pokemons = Array.from({length: 35}, (_, k) => ({
+    name: 'Pokémon name',
+    id: k + 1,
+  }))
   return (
     <SafeAreaView style={[styles.container, {backgroundColor: colors.tint}]}>
-      <Card>
-        <ThemedText variant="headline" color="grayDark">Pokédex</ThemedText>
+      <View style={styles.header}>
+        <Image source={require("@/assets/images/pokeball.png")} width={24} height={24}/>
+        <ThemedText variant="headline" color="grayWhite">Pokédex</ThemedText>
+      </View>
+      <Card style={styles.body}>
+        <FlatList 
+        data={pokemons} 
+        numColumns={3}
+        contentContainerStyle={[styles.gridGap, styles.list]}
+        columnWrapperStyle={styles.gridGap}
+        renderItem={({item}) => <Card style={{flex: 1/3, height: 200}}>
+          <Text>{item.name}</Text>
+        </Card>} keyExtractor={(item) => item.id.toString()}/>
       </Card>
     </SafeAreaView>
   );
@@ -18,5 +33,21 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 4,
+  },
+  header: {
+    flexDirection: "row", 
+    alignItems: "center",
+    gap: 16,
+    padding: 12,
+  },
+  body: {
+    flex: 1,
+  },
+  gridGap: {
+    gap: 8,
+  },
+  list:{
+    padding: 12,
   }
 })
--- Pokenative/components/Card.tsx
+++ Pokenative/components/Card.tsx
@@ -5,14 +5,13 @@ import { View, type ViewProps, type ViewStyle } from "react-native";
 type Props = ViewProps
 
 
-export function Card({ style, ...rest }: Props) {
+export function Card({style, ...rest}: Props) {
     const colors = useThemeColors()
     return <View style={[style, styles, { backgroundColor: colors.grayWhite }]} {...rest}/>
 
 }
 
 const styles = {
-    backgroundColor: "#FFFFFF",
     borderRadius: 8,
     ...Shadows.dp2
 } satisfies ViewStyle
