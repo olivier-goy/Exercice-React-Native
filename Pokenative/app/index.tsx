422e87c7a1c42d385312eaabd6f2da9418503384
Olivier GOY <olivier.goy.37@gmail.com>
2025-07-21 12:13:17 +0200

    add searchbar

--- Pokenative/app/index.tsx
+++ Pokenative/app/index.tsx
@@ -1,7 +1,10 @@
 import { useThemeColors } from '@/app/pokemon/hooks/useThemeColors';
 import { Card } from '@/components/Card';
+import { Row } from '@/components/Row';
+import { SearchBar } from '@/components/SearchBar';
 import { ThemedText } from '@/components/ThemedText';
-import { ActivityIndicator, FlatList, Image, StyleSheet, View } from "react-native";
+import { useState } from 'react';
+import { ActivityIndicator, FlatList, Image, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { PokemonCard } from './pokemon/constants/pokemon/PokemonCard';
 import { getPokemonId } from './pokemon/functions/pokemon';
@@ -11,13 +14,16 @@ export default function Index() {
   const colors = useThemeColors();
   const {data, isFetching, fetchNextPage} = useInfinitFetchQuery("/pokemon?limit=21")
   const pokemons = data?.pages.flatMap(page => page.results) ?? []
-  
+  const [search, setSearch] = useState(" ")
   return (
     <SafeAreaView style={[styles.container, {backgroundColor: colors.tint}]}>
-      <View style={styles.header}>
+      <Row style={styles.header} gap={16}>
         <Image source={require("@/assets/images/pokeball.png")} width={24} height={24}/>
         <ThemedText variant="headline" color="grayWhite">Pokédex</ThemedText>
-      </View>
+      </Row>
+      <Row>
+        <SearchBar value={search} onChange={setSearch} />
+      </Row>
       <Card style={styles.body}>
         <FlatList 
         data={pokemons} 
@@ -40,13 +46,12 @@ const styles = StyleSheet.create({
     padding: 4,
   },
   header: {
-    flexDirection: "row", 
-    alignItems: "center",
-    gap: 16,
-    padding: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
   },
   body: {
     flex: 1,
+    marginTop: 16,
   },
   gridGap: {
     gap: 8,
---
+++ Pokenative/components/SearchBar.tsx
@@ -0,0 +1,42 @@
+import { useThemeColors } from "@/app/pokemon/hooks/useThemeColors";
+import { Image, StyleSheet, TextInput } from "react-native";
+import { Row } from "./Row";
+
+type Props = {
+    value: string,
+    onChange: (s: string) => void,
+}
+
+export function SearchBar({value, onChange}: Props) {
+    const colors = useThemeColors()
+    return (
+        <Row gap={8} style={[styles.wrapper, { backgroundColor: colors.grayWhite }]}>
+            <Image 
+            source={require("@/assets/images/search.png")} 
+            width={16} 
+            height={16} 
+            />
+            <TextInput 
+            onChangeText={onChange} 
+            value={value} 
+            style={styles.input} 
+            placeholder="SEARCH"
+            />
+        </Row>
+        )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        borderRadius: 16,
+        height: 32,
+        paddingHorizontal: 12,
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        fontSize: 11,
+        lineHeight: 16,
+    }
+})
